1927cd45785f5bc6a0a1d33c7494fcc829dba5b9
carlos-farfan <79650604+carlos-farfan@users.noreply.github.com>
2021-04-10 05:27:48 -0500

    npmYnode

--- semana7/dia4/npmYnode/ejemplo.js
+++ semana7/dia4/npmYnode/ejemplo.js
@@ -5,3 +5,12 @@ const lista = require("./array")
 lista.map((elemento)=>{
     console.log(elemento)
 })
+
+
+let cowsay = require("cowsay")
+
+console.log(cowsay.say({
+    text:"Hola amigos de codigo !!!.........",
+    e : "oO",
+    T : "U"
+}))
